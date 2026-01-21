ee438cbc134e3c5833521598b623c1e0db839f02
Shivam Mac <shivam.gupta.23112003@gmail.com>
2026-01-21 23:47:25 +0530

    Added Dashboard

---
+++ frontend/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import DashboardPage from "@wallet/pages/Dashboard";
+
+
+export default function Dashboard() {
+  return (
+    <main className="min-h-full w-full overflow-hidden pt-20 px-[3vw]">
+      <DashboardPage />
+    </main>
+  );
+}
